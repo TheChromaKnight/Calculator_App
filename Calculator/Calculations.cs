12a07bcdefe56a72a219cf5d2206037ba5199e34
TheChromaKnight <bebb99@gmail.com>
2019-07-31 21:05:49 +0200

    Completed the design and the addition part is halfway complete Other functions need integration but they are also halfway there

---
+++ Calculator/Calculations.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorFunctions
+{
+    public static class Calculations
+    {
+        //The array is used as a memory,so it can be used to check the previous or next element (if there is any)
+        //100 because the 0th element is always 0... I used it as I needed a Previous number for the calculations as the index can't be -1
+        static double[] Memory = new double[100];
+        
+        
+        
+        //Element of the Memory Array
+        static int MemoryElement = 1;
+
+        //The current number is always the last updated element of the Memory array
+         static double CurrentNumber;
+        //The previous number is always the penultimate updated element of the Memory array
+         static double PreviousNumber;
+
+        
+
+
+        //Convert String to Double
+        public static double ConvertStringToDouble(string Number)
+        {
+            double ConvertedNumber = Convert.ToDouble(Number);
+
+            return ConvertedNumber;
+        }
+
+        
+
+
+       //Calculation part
+
+        public static void GiveBack()
+        {
+            for(int i = 0; i< Memory.Length; i++)
+            {
+                Console.WriteLine(Memory[i]);
+            }
+            
+            
+        }
+
+        //Addition
+
+            //STOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOP
+            //Need to complete this function so that it can infinitely use addition like (3+3+3+3+3 is 15 and not 6 as only the last 2 numbers matter "for now" )
+        public static double Addition()
+        {
+            CurrentNumber = Memory[MemoryElement];
+            PreviousNumber = Memory[MemoryElement -1];
+            
+            MemoryElement += 1;
+            return  (CurrentNumber+ PreviousNumber);
+        }
+
+        //Subtraction
+        public static double Subtraction()
+        {
+            CurrentNumber = Memory[MemoryElement];
+            PreviousNumber = Memory[MemoryElement - 1];
+            MemoryElement += 1;
+            return (CurrentNumber - PreviousNumber);
+        }
+
+        //Division
+        public static double Division()
+        {
+            CurrentNumber = Memory[MemoryElement];
+            PreviousNumber = Memory[MemoryElement - 1];
+            MemoryElement += 1;
+            return (CurrentNumber / PreviousNumber);
+        }
+
+        //Multiplication
+        public static double Multiplication()
+        {
+            CurrentNumber = Memory[MemoryElement];
+            PreviousNumber = Memory[MemoryElement-1];
+            MemoryElement += 1;
+            return (CurrentNumber * PreviousNumber);
+            
+        }
+
+        //Summarize
+        //public static double Summarize()
+        //{
+        //Arra
+        //}
+
+
+
+
+
+
+        //Memory get/set/clear functions/methods
+        
+        //Add to Memory array part
+        public static void AddToMemory(double Number)
+        {
+            Memory[MemoryElement] = Number;
+            //MemoryElement += 1;
+        }
+
+        //Return penultimate value of Memory array
+        public static string  ReturnPreviousElementOfMemory()
+        {
+            return Memory[MemoryElement - 1].ToString();
+        }
+
+        //Return next value of Memory array
+        public static string ReturnNextElementOfMemory()
+        {
+            return Memory[MemoryElement + 1].ToString();
+        }
+
+        //Clear full Memory array
+        public static void ClearFullMemory()
+        {
+            Array.Clear(Memory, 0, 100);
+            MemoryElement = 0;
+        }
+
+        //Clear last element of Memory array
+        public static void ClearLastElementOfMemory()
+        {
+            Array.Clear(Memory,MemoryElement ,1);
+            MemoryElement -= 1;
+        }
+
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalculatorFunctions;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //0 means new calculations and not in progress
+        //e.g 2+2+2*2 means progress will be 1 as long as we don't get the final result with = sign
+        int CalculationIsOnGoing = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TbInputNumbers.Text = "0";
+            TbMemory.Text = "";
+            
+            
+        }
+
+        //button 0
+        private void Btn0_Click(object sender, RoutedEventArgs e)
+        {
+           
+                if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+                {
+                    TbInputNumbers.Text = "0";
+                    CalculationIsOnGoing = 0;
+                }
+                else if (TbInputNumbers.Text.Length == 10)
+                {
+
+                }
+                else
+                    TbInputNumbers.Text += "0";
+            
+           
+        }
+
+        //button 1
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "1";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "1";
+        }
+
+        //button 2
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "2";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "2";
+        }
+
+        //button 3
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "3";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "3";
+        }
+
+        //button 4
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "4";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "4";
+        }
+
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "5";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "5";
+        }
+
+        //button 6
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "6";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "6";
+        }
+
+        //button 7
+        private void Btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "7";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "7";
+        }
+
+        //button 8
+        private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "8";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "8";
+        }
+
+        //button 9
+        private void Btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (TbInputNumbers.Text == "0" || CalculationIsOnGoing == 1)
+            {
+                TbInputNumbers.Text = "9";
+                CalculationIsOnGoing = 0;
+            }
+            else if (TbInputNumbers.Text.Length == 10)
+            {
+
+            }
+            else
+                TbInputNumbers.Text += "9";
+        }
+
+        private void BtnAddition_Click(object sender, RoutedEventArgs e)
+        {
+
+            double CurrentNumber = Convert.ToDouble(TbInputNumbers.Text);
+            //MessageBox.Show("Current num:" + CurrentNumber);
+
+            Calculations.AddToMemory(CurrentNumber);
+
+            TbMemory.Text = CurrentNumber.ToString();
+
+            double Result = Calculations.Addition();
+
+            TbInputNumbers.Text = Result.ToString();
+
+            //MessageBox.Show(Result.ToString());
+
+            //Calculations.GiveBack();
+
+            CalculationIsOnGoing = 1;
+
+        }
+    }
+}
